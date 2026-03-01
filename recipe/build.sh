45126355581b5c2df7aa61f9cf188592979d58b8
UENO, M. <eunosium-1128@live.jp>
2026-03-01 22:10:42 +0900

    Add PostgreSQL configuration to build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,7 +8,10 @@ cmake ${CMAKE_ARGS} .. \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_PREFIX_PATH=$PREFIX \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DBUILD_SHARED_LIBS=ON
+      -DBUILD_SHARED_LIBS=ON \
+      -DPostgreSQL_ROOT=$PREFIX \
+      -DPostgreSQL_LIBRARY=$PREFIX/lib/libpq${SHLIB_EXT} \
+      -DPostgreSQL_INCLUDE_DIR=$PREFIX/include
 
 cmake --build . --config Release
 cmake --build . --config Release --target install
